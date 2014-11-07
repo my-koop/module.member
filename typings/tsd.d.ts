f0292b55d9d64832fedbb3aeb7640f8cfc17d3ba
Cellule <mike.ferris@hotmail.com>
2014-11-06 23:16:42 -0500

    fix typings

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -3,3 +3,6 @@
 /// <reference path="mykoop-member/mykoop-member.d.ts" />
 /// <reference path="../node_modules/mykoop-utils/typings/mykoop-utils/mykoop-utils.d.ts" />
 /// <reference path="express/express.d.ts" />
+/// <reference path="mysql/mysql.d.ts" />
+/// <reference path="mykoop-database/mykoop-database.d.ts" />
+/// <reference path="async/async.d.ts" />
