f92964b1f9b3f3dd2c561951205f5d34bdcb29ef
ad <stepanian.adam@live.com>
2014-11-18 10:07:11 -0500

    Removed testing route

--- metadata/routes.ts
+++ metadata/routes.ts
@@ -1,9 +1,4 @@
 import utils = require("mykoop-utils");
 export function addRoutes(metaData: utils.MetaDataBuilder) {
-  metaData.addFrontendRoute({
-    idPath: ["public","newMemBox"],
-    component: "NewMemberBox",
-    name: "newMemBox",
-    path: "/newMemBox"
-  });
+
 }
